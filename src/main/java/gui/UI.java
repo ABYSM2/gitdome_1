81496c5862ebfc1ab385270c4c77631ffc2f7169
wxy76 <762768764@qq.com>
2024-12-31 02:38:56 +0800

    1.0.0

--- src/main/java/gui/UI.java
+++ src/main/java/gui/UI.java
@@ -1,4 +1,14 @@
 package gui;
 
-public class UI {
+import javax.swing.*;
+
+public class UI extends JFrame {
+    public UI() {
+        setTitle("测试");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
 }
