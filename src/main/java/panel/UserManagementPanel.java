f053f68cd4ada376460e2f5e64166ec25d1713d4
wxy76 <762768764@qq.com>
2024-12-31 15:52:23 +0800

    1.0.0

---
+++ src/main/java/panel/UserManagementPanel.java
@@ -0,0 +1,11 @@
+package panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UserManagementPanel extends JPanel {
+    public UserManagementPanel() {
+        setBackground(Color.LIGHT_GRAY);
+        add(new JLabel("用户管理面板"));
+    }
+}
